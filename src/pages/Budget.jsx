ef043ae91572651b87a4f6ad409e19b0fa09c121
SOHAN-JAGADEESH <sohanjagadeesh@gmail.com>
2023-10-10 12:43:17 +1100

    budget functionlity
    all features working

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -9,8 +9,36 @@ const Budget = () => {
   const [income, setIncome] = useState('');
   const [savings, setSavings] = useState('');
   const [months, setMonths] = useState('');
+  const [savings1, setSavings1] = useState(null);
+  const [savings2, setSavings2] = useState(null);
 
-  
+  const [errors, setErrors] = useState({ income: false, savings: false, months: false });
+
+  const validateInput = (value, type) => {
+    switch(type) {
+      case 'income':
+        return value >= 0 && value <= 200000;
+      case 'savings':
+        return value > 0 && value <= 200000;
+      case 'months':
+        return value > 0 && value < 48 ;
+      default:
+        return false;
+    }
+  }
+
+  const handleInputChange = (e, type) => {
+    const value = e.target.value;
+    if (validateInput(value, type)) {
+      setErrors(prevState => ({ ...prevState, [type]: false }));
+      if (type === 'income') setIncome(value);
+      if (type === 'savings') setSavings(value);
+      if (type === 'months') setMonths(value);
+    } else {
+      setErrors(prevState => ({ ...prevState, [type]: true }));
+    }
+  }
+
 
   const API_ENDPOINT = 'https://backend-btlp.onrender.com/poem';
 
@@ -34,7 +62,7 @@ const sendDataToAPI = async () => {
     switch (choice) {
       case 'saveMoney':
         promptText += `
-          and their desire to save money with an income of ${income} and a savings goal of 10%,
+          and their desire to save money with an income of ${income} and a savings goal of ${savings1}%,
           provide a custom budget recommendation to help them save money.
         `;
         break;
@@ -46,7 +74,7 @@ const sendDataToAPI = async () => {
         break;
       case 'reduceExpenditure':
         promptText += `
-          and their aim to reduce expenditure by ${savings}%,
+          and their aim to reduce expenditure by ${savings2}%,
           provide a custom budget recommendation to help them reduce their spending.
         `;
         break;
@@ -106,62 +134,140 @@ const sendDataToAPI = async () => {
       <div className={`${styles.paddingX} ${styles.flexCenter} main-section `}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
-          <div className={`mt-8 text-center bg-gray-500 p-5 rounded`}>
+          <h2 className={`${styles.heading2} mt-5 text-center`}>
+              Use AI To Supercharge Your Budgeting
+            </h2>
+            <p className={`${styles.paragraph} mt-5 text-center`}>
+              It could be challenging to evaluate the purchasing power of your money in a foreign country.
+              We are here to help,
+              Enter any amount of Australian Dollar here and see what it can get you.
+              Every dollar spent is a reminder of the sacrifices made by your family miles away, spend it wisely!
+            </p>
+          <div className={`mt-8 text-center border-2 border-[#1CE8A8] p-5 rounded`}>
             <h2 className={styles.heading2}>
                 Budgeting Options
             </h2>
             <p className={`${styles.paragraph} mt-5`}>
                 Choose your budgeting goal:
             </p>
-
-            <button onClick={() => setChoice('saveMoney')} className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Save Money</button>
-            <button onClick={() => setChoice('saveGoal')} className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Save Towards a Goal</button>
-            <button onClick={() => setChoice('reduceExpenditure')} className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Reduce Expenditure</button>
+          
+            <button onClick={() => setChoice('saveMoney')} className={`p-3 m-2 rounded ${choice === 'saveMoney' ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>Save Money</button>
+            <button onClick={() => setChoice('saveGoal')}className={`p-3 m-2 rounded ${choice === 'saveGoal' ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>Save Towards a Goal</button>
+            <button onClick={() => setChoice('reduceExpenditure')} className={`p-3 m-2 rounded ${choice === 'reduceExpenditure' ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>Reduce Expenditure</button>
 
             
 
             {choice === 'saveMoney' && (
               <div className="mt-5">
-                <input type="number" placeholder="Income in $" value={income} onChange={e => setIncome(e.target.value)} className="border p-2 bg-gray-200 m-2"/>
+                <label className={`${styles.paragraph} block mb-2`}>Enter your Income:</label>
+                <input 
+                  type="number" 
+                  placeholder="Income in $" 
+                  value={income} 
+                  onChange={e => handleInputChange(e, 'income')} 
+                  className={`border p-2 bg-gray-200 m-2 ${errors.income ? 'border-red-500' : ''}`}
+                />
+                {errors.income && <p className="text-red-500">Income should be between 0 and 200,000</p>}
                 <div>
-                  <button className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">10%</button>
-                  <button className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">20%</button>
-                  <button className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Custom</button>
+                  <br/>
+                  <label className={`${styles.paragraph} block mb-2`}>Select how much you want to save:</label>
+                    <button onClick={() => setSavings1(10)} className={`p-3 m-2 rounded ${savings1 === 10 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>10%</button>
+                    <button onClick={() => setSavings1(20)} className={`p-3 m-2 rounded ${savings1 === 20 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>20%</button>
+                    <button onClick={() => setSavings1(30)} className={`p-3 m-2 rounded ${savings1 === 30 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>30%</button>
+                    <button onClick={() => setSavings1(40)} className={`p-3 m-2 rounded ${savings1 === 40 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>40%</button>
+                            
                 </div>
               </div>
             )}
 
             {choice === 'saveGoal' && (
               <div className="mt-5">
-                <input type="number" placeholder="Income in $" value={income} onChange={e => setIncome(e.target.value)} className="border p-2 bg-gray-200 m-2"/>
-                <input type="number" placeholder="Savings in $" value={savings} onChange={e => setSavings(e.target.value)} className="border p-2 bg-gray-200 m-2"/>
-                <input type="number" placeholder="Months to achieve" value={months} onChange={e => setMonths(e.target.value)} className="border p-2 bg-gray-200 m-2"/>
+                <label className={`${styles.paragraph} block mb-2`}>Enter your Income:</label>
+                <input 
+                  type="number" 
+                  placeholder="Income in $" 
+                  value={income} 
+                  onChange={e => handleInputChange(e, 'income')} 
+                  className={`border p-2 bg-gray-200 m-2 ${errors.income ? 'border-red-500' : ''}`}
+                />
+                {errors.income && <p className="text-red-500">Income should be between $0 and $200,000</p>}
+                
+                <label className={`${styles.paragraph} block mb-2`}>Enter your Savings goal in AUD:</label>
+                <input 
+                  type="number" 
+                  placeholder="Savings in $" 
+                  value={savings} 
+                  onChange={e => handleInputChange(e, 'savings')} 
+                  className={`border p-2 bg-gray-200 m-2 ${errors.savings ? 'border-red-500' : ''}`}
+                />
+                {errors.savings && <p className="text-red-500">Savings goal should be between $0 and $200,000</p>}
+                
+                <label className={`${styles.paragraph} block mb-2`}>Enter number of months for the goal:</label>
+                <input 
+                  type="number" 
+                  placeholder="Months to achieve" 
+                  value={months} 
+                  onChange={e => handleInputChange(e, 'months')} 
+                  onKeyPress={e => (e.key === '.' && e.preventDefault())}
+                  className={`border p-2 bg-gray-200 m-2 ${errors.months ? 'border-red-500' : ''}`}
+                />
+                {errors.months && <p className="text-red-500">Months should be between 1 and 47 and be a whole number</p>}
               </div>
             )}
 
-            {choice === 'reduceExpenditure' && (
+
+          {choice === 'reduceExpenditure' && (
               <div className="mt-5">
-                <button className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Reduce by 10%</button>
-                <button className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Reduce by 20%</button>
-                <button className="bg-gray-500 text-[#1CE8A8] p-3 m-2 rounded">Custom Reduction</button>
+                <label className={`${styles.paragraph} block mb-2`}>Select by how much you want to reduce your expenditure:</label>
+                <button 
+                  onClick={() => setSavings2(10)}
+                  className={`p-3 m-2 rounded ${savings2 === 10 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>
+                  10%
+                </button>
+                <button 
+                  onClick={() => setSavings2(20)}
+                  className={`p-3 m-2 rounded ${savings2 === 20 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>
+                  20%
+                </button>
+                <button 
+                  onClick={() => setSavings2(30)}
+                  className={`p-3 m-2 rounded ${savings2 === 30 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>
+                  30%
+                </button>
+                {/* For custom reduction, you might need an additional input field to get the exact percentage. For now, I'm leaving it as a button. */}
+                <button 
+                  onClick={() => setSavings2(40)}
+                  className={`p-3 m-2 rounded ${savings2 === 40 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>
+                  40%
+                </button>
+                <button 
+                  onClick={() => setSavings2(50)}
+                  className={`p-3 m-2 rounded ${savings2 === 50 ? 'bg-[#1CE8A8] text-gray-500' : 'bg-gray-500 text-[#1CE8A8]'}`}>
+                  50%
+                </button>
               </div>
             )}
-          </div>
-          <button onClick={sendDataToAPI} className="bg-[#1CE8A8] p-3 m-2 rounded">Get Custom Budget</button>
           
-          <div className="mt-8 ">
-            <h2 className={styles.heading2}>
-                Your Custom Budget Recommendation
-            </h2>
-            <div className={`${styles.paragraph} mt-5 text-white wordWrap`} style={{ maxWidth: '100%', overflow: 'hidden' }}>
-                <pre className="preWrap">{budgetRecommendation}</pre>
+
+            <button onClick={sendDataToAPI} className="bg-[#1CE8A8] p-3 m-2 rounded">
+                Get Custom Budget
+            </button>
+          </div>
+            <div className="mt-8 ">
+                <h2 className={styles.heading2}>
+                    Your Custom Budget Recommendation
+                </h2>
+                <div className={`${styles.paragraph} mt-5 text-white wordWrap`} style={{ maxWidth: '100%', overflow: 'hidden' }}>
+                    <pre className="preWrap">{budgetRecommendation}</pre>
+                </div>
             </div>
-        </div>
-        </div>
+       
       </div>
-      <br/> <br/> <br/> <br/>
-      <Footer/>
     </div>
+    <br/> <br/> <br/> <br/>
+    <Footer/>
+</div>
+
   );
 }
 
